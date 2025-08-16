9ea995d73d8eda42c1eb85e0d30e2d049e1ddfef
Wilson Medeiros <wilson.medeiros@mindera.pt>
2025-08-16 20:06:52 +0100

    fix: build application

--- app/api/disable-draft/route.ts
+++ app/api/disable-draft/route.ts
@@ -1,6 +1,11 @@
 import { draftMode } from "next/headers";
 
 export async function GET(request: Request) {
-  draftMode().disable();
+  const draft = await draftMode();
+  if (!draft.isEnabled) {
+    return new Response("Draft mode is already disabled", { status: 400 });
+  } else {
+    draft.disable();
+  }
   return new Response("Draft mode is disabled");
 }
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -17,13 +17,18 @@ export async function generateStaticParams() {
   }));
 }
 
+type PageProps = {
+  params: { slug: string };
+};
+
 export default async function PostPage({
   params,
 }: {
-  params: { slug: string };
+  params: Promise<{ slug: string }>
 }) {
-  const { isEnabled } = draftMode();
-  const { post, morePosts } = await getPostAndMorePosts(params.slug, isEnabled);
+  const { isEnabled } = await draftMode();
+  const { slug } = await params;
+  const { post, morePosts } = await getPostAndMorePosts(slug, isEnabled);
 
   return (
     <div className="container mx-auto px-5">
